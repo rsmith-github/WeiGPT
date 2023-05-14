a655c95ce3e2c33174944c4847d22c74b2f1aa28
rsmith-github <rsmith.git@gmail.com>
2023-05-14 15:17:52 +0100

    feat: working MVP

--- chatGPT/settings.py
+++ chatGPT/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = 'django-insecure-==_5gu3^tpf2#d8%fgvuv=)9lq-^!0z1y&fbwxhmhluf0!zlud
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["172.20.10.9", "127.0.0.1"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -79,6 +80,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+AUTH_USER_MODEL = "main.User"
 
 
 # Password validation
--- main/views.py
+++ main/views.py
@@ -1,7 +1,42 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
 
-
+@login_required(login_url='login')
 def index(request):
-    return render(request, "main/index.html")
+    result = request.GET.get("result")
+
+    gradio_url = "http://http://127.0.0.1:7860/"
+
+    return render(
+        request,
+        "main/index.html",
+        {"result": result, "gradio_url": gradio_url},
+    )
+
+
+def login_view(request):
+    if request.method == "POST":
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "main/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "main/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
