615ad52e3b58ad785b39eb829136627647ac38f4
rsmith-github <rsmith.git@gmail.com>
2023-05-15 19:23:26 +0100

    feat: allow model to 'remember'

--- main/views.py
+++ main/views.py
@@ -10,28 +10,30 @@ from decouple import config
 
 openai.api_key = config('OPENAI_API_KEY')
 
+# Allow ai model to 'remember'
+knowledge_cutoff = "2021-09"  # Replace with your desired knowledge cutoff
+current_date = datetime.datetime.now().strftime(
+    "%Y-%m-%d")  # Get the current date
+starting_prompt = f"You are ChatGPT, a large language model trained by OpenAI. Act as a personal assistant for Chinese pop-star Wei Wei (韦唯). Answer as concisely as possible. Respond only in Simplified Chinese. Knowledge cutoff: {knowledge_cutoff} Current date: {current_date}."
+messages = [
+    {"role": "system", "content": starting_prompt},
+    {"role": "user", "content": "Who are you?"},
+    {"role": "assistant", "content": "我是您的私人AI助理！"},
+    {"role": "user", "content": "很高兴认识！"},
+    {"role": "assistant", "content": "彼此！"},
+]
+
 # Get AI response based on user prompt using model gpt-3.5-turbo.
 def generate_responses(user_prompt):
-    knowledge_cutoff = "2021-09"  # Replace with your desired knowledge cutoff
-    current_date = datetime.datetime.now().strftime(
-        "%Y-%m-%d")  # Get the current date
-    starting_prompt = f"You are ChatGPT, a large language model trained by OpenAI. Act as a personal assistant for Chinese pop-star Wei Wei (韦唯). Answer as concisely as possible. Respond only in Simplified Chinese. Knowledge cutoff: {knowledge_cutoff} Current date: {current_date}. {user_prompt}"
-
+    messages.append({"role": "user", "content": user_prompt})
     # get response
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": starting_prompt},
-            {"role": "user", "content": "Who are you?"},
-            {"role": "assistant", "content": "我是您的私人AI助理！"},
-            {"role": "user", "content": "很高兴认识！"},
-            {"role": "assistant", "content": "彼此！"},
-            {"role": "user", "content": user_prompt},
-        ]
+        messages=messages
     )
-
     # return response
     generated_text = response['choices'][0]['message']['content'].strip()
+    messages.append({"role": "assistant", "content": generated_text})
     return generated_text
 
 
@@ -50,6 +52,8 @@ def index(request):
     return render(request, "main/index.html")
 
 # User login
+
+
 def login_view(request):
     if request.method == "POST":
         # Attempt to sign user in
@@ -69,6 +73,8 @@ def login_view(request):
         return render(request, "main/login.html")
 
 # User logout
+
+
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse("index"))
