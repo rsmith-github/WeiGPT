e9c920847257ed788d71bc610c9804f33e55f25b
rsmith-github <rsmith.git@gmail.com>
2023-05-15 18:44:20 +0100

    feat: add loading icon and fix touch event for 'send' button.

--- main/static/main/js/async.js
+++ main/static/main/js/async.js
@@ -1,10 +1,10 @@
-
-
 const userOnSubmit = () => {
     let submitButton = document.getElementById("send-chat-btn");
-    submitButton.addEventListener("click", async (event) => {
+
+    const action = async (event) => {
         event.preventDefault();
         let responseDiv = document.getElementById("response");
+
         let promise = await fetchResponse(document.getElementById("user-prompt").value);
         if (promise) {
             // populate response section
@@ -12,14 +12,24 @@ const userOnSubmit = () => {
             responseDiv.innerHTML += promise.response.replaceAll("\n", "<br>");
             responseDiv.innerHTML += "<br><br>";
         }
-    })
+    }
+
+    submitButton.addEventListener("click", action);
+    submitButton.addEventListener("touchstart", action);
 }
 
 userOnSubmit()
 
-async function fetchResponse(user_prompt) {
+function fetchResponse(user_prompt) {
     const csrftoken = document.querySelector("[name='csrfmiddlewaretoken']").value;
-    const response = await fetch("http://localHost:8000/", {
+
+    // Get loading element and show it
+    let loadingDiv = document.createElement("div");
+    loadingDiv.innerHTML = "<img src='static/main/loading.gif' style='width: 50px;'/>"
+
+    document.getElementById("response").append(loadingDiv)
+
+    return fetch("/", {
         method: "POST",
         headers: {
             Accept: "application/json",
@@ -29,7 +39,19 @@ async function fetchResponse(user_prompt) {
         body: JSON.stringify({
             "user_prompt": user_prompt,
         })
-    });
-    const jsonData = await response.json();
-    return jsonData
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("HTTP error " + response.status);
+            }
+            return response.json();
+        })
+        .catch(error => {
+            console.log("An error occurred: " + error.message);
+            // You can handle error state here
+        })
+        .finally(() => {
+            // Hide loading element when promise is either resolved or rejected
+            loadingDiv.style.display = "none";
+        });
 }
